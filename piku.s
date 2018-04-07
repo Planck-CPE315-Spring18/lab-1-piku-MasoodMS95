f10aac6da8645da9d1752b60d91e1e7ff89fdcee
msaaed <masoodsaaed@gmail.com>
2018-04-07 15:28:47 -0700

    Final

--- piku.s
+++ piku.s
@@ -8,7 +8,11 @@ main:
     push    {ip, lr}
 
     @ Load the argument and perform the call. This is like 'printf("...")' in C.
-    ldr     r0, =message
+    ldr     r0, =haiku1
+    bl      printf
+    ldr     r0, =haiku2
+    bl      printf
+    ldr     r0, =haiku3
     bl      printf
 
     @ Exit from 'main'. This is like 'return 0' in C.
@@ -20,5 +24,9 @@ main:
     @ --------------------------------
     @ Data for the printf call. The GNU assembler's ".asciz" directive
     @ automatically adds a NULL character termination.
-message:
-    .asciz  "Hello, world.\n"
+haiku1:
+    .asciz  "I sure do love pi.\n"
+haiku2:
+    .asciz "It makes me feel good inside.\n"
+haiku3:
+    .asciz "Too bad arm is bad.\n"
